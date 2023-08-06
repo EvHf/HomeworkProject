2de23a236665d41cb7196a19441e07c0338e8711
EvHf <evdchnk@gmail.com>
2023-08-06 16:13:49 +0300

    SolveConflict

--- Assets/FFF1.cs
+++ Assets/FFF1.cs
@@ -7,7 +7,7 @@ public class FFF1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //conflict 1
+
     }
 
     // Update is called once per frame
